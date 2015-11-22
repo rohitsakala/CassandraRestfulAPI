81dca0ed09e61dafa6782fa11d89c6ed6a9b9335
rohitsakala <rohitsakala@gmail.com>
2015-11-23 02:15:38 +0530

    Completed half project

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from flask import Flask,request
 from flask import render_template
 from flask import jsonify
+from flask import redirect
+from flask import Blueprint
+from flask.ext.paginate import Pagination
 from flask.ext.mongoengine import MongoEngine
 from pymongo import read_preferences
 from flask.ext.mongoengine.wtf import model_form
+from math import ceil
 import json
 import libvirt
 import os
@@ -19,21 +23,69 @@ from cassandra.query import SimpleStatement
 
 app = Flask(__name__)
 
+######## Global Variables #############
+PER_PAGE = 10
+
+################ Class for Pagination ###################
+
+from math import ceil
+
+class Pagination(object):
+
+    def __init__(self, page, per_page, total_count):
+        self.page = page
+        self.per_page = per_page
+        self.total_count = total_count
+
+    @property
+    def pages(self):
+        return int(ceil(self.total_count / float(self.per_page)))
+
+    @property
+    def has_prev(self):
+        return self.page > 1
+
+    @property
+    def has_next(self):
+        return self.page < self.pages
+
+    def iter_pages(self, left_edge=2, left_current=2,
+                   right_current=5, right_edge=2):
+        last = 0
+        for num in xrange(1, self.pages + 1):
+            if num <= left_edge or \
+               (num > self.page - left_current - 1 and \
+                num < self.page + right_current) or \
+               num > self.pages - right_edge:
+                if last + 1 != num:
+                    yield None
+                yield num
+                last = num
+
+
+
+
+
 @app.route('/')
 def index():
 	return "Cloud Main Project - Exposing Cassandra Data Tables are Restful API's"
 
 ####################### Code related to KeySpace Operations #######################
 
-@app.route('/keyspaces/', methods=['GET'])
-def getKeyspaces():
-	cluster = Cluster()
+@app.route('/keyspaces/', defaults={'page': 1}, methods=['GET'])
+@app.route('/keyspaces/page/<int:page>', methods=['GET'])
+def getKeyspaces(page):
+   	cluster = Cluster()
 	session = cluster.connect('system')
 	rows = session.execute('select keyspace_name from schema_keyspaces')
 	keyspaces=[]
 	for x in range(len(rows)):
-	    keyspaces.append(rows[x][0])
-	return render_template('listkeyspace.html',keyspaces=keyspaces)
+		keyspaces.append(rows[x][0])
+    	pages = keyspaces[(page-1)*PER_PAGE:PER_PAGE*page]
+    	if not pages and page != 1:
+   		abort(404)
+    	pagination = Pagination(page, PER_PAGE, len(rows))
+   	return render_template('listkeyspace.html',pagination=pagination,pages=pages,section = 'getKeyspaces')
 
 @app.route('/keyspaces/<keyspaceid>', methods=['GET'])
 def getKeyspaceInfo(keyspaceid):
@@ -48,7 +100,6 @@ def getKeyspaceInfo(keyspaceid):
 			info['replication_factor'] = 0
 			return jsonify(keyspace=info)
 		info['replication_factor'] = rows[int(keyspaceid) -1][3]
-		info['replication_factor'] = json.loads(info['replication_factor'])['replication_factor']
 		return render_template('keyspaceinfo.html',info=info)
 	else:
 		return render_template('error.html',error="Not a valid keyspace")
@@ -107,21 +158,27 @@ def deleteKeyspace(keyspaceid):
 
 ######################### Code related to adding machine to the cluster or increasing the nodes of the cluster #####################
 
-@app.route('/nodes/', methods=['GET'])
-def getNodes():
+@app.route('/nodes/', defaults={'page': 1}, methods=['GET'])
+@app.route('/nodes/page/<int:page>', methods=['GET'])
+def getnodes(page):
 	cluster = Cluster()
 	session = cluster.connect('system')
 	rows = session.execute('select listen_address from local')
-	info = {}
+	info = []
 	c = 0
 	for x in range(len(rows)):
-		info[c+1] = rows[x][0]
+		info.append(rows[x][0])
 		c=c+1
 	rows = session.execute('select peer from peers')
 	for x in range(len(rows)):
-		info[c+1] = rows[x][0]
+		info.append(rows[x][0])
 		c=c+1
-	return jsonify(Info=info)
+	pages = info[(page-1)*PER_PAGE:PER_PAGE*page]
+    	if not pages and page != 1:
+   		abort(404)
+    	pagination = Pagination(page, PER_PAGE, len(info))
+   	return render_template('listnode.html',pagination=pagination,pages=pages,section = 'getNodes')
+	
 
 @app.route('/nodes/<nodeid>', methods=['GET'])
 def getNode(nodeid):
@@ -140,9 +197,10 @@ def getNode(nodeid):
 	if int(nodeid) <= c and int(nodeid) > 0:
 		data = {}
 		data['name'] = info[int(nodeid)]
-		return jsonify(Data=data)
+		data['nodeid'] = nodeid
+		return render_template('nodeinfo.html',info=data)
 	else:
-		return jsonify(error="No such nodeid")
+		return render_template('error.html',error="Not a valid nodeid")
 
 @app.route('/nodes/', methods=['POST'])
 def createNode():
@@ -250,6 +308,15 @@ def createColumnFamily(keyspaceid):
    #     tags set<text>,
   #      data blob
  #   );
+
+def url_for_other_page(page):
+    args = request.view_args.copy()
+    args['page'] = page
+    return url_for(request.endpoint, **args)
+
+app.jinja_env.globals['url_for_other_page'] = url_for_other_page
+
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
--- test.py
+++ test.py
@@ -4,8 +4,9 @@ from cassandra.query import SimpleStatement
 from threading import Event, Lock, Thread
 
 cluster = Cluster()
-session = cluster.connect('system')
-query = "SELECT * FROM local"  # users contains 100 rows
+session = cluster.connect('rohit')
+query = "SELECT * FROM users"  # users contains 100 rows
 statement = SimpleStatement(query, fetch_size=10)
 for user_row in session.execute(statement):
     print user_row
+    print "asdfs"
